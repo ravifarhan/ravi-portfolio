4cfcfc5d6029858f84b1d6503b0d8a56e6868aef
ravifarhan <mravifarhan02@gmail.com>
2024-06-26 21:26:56 +0700

    fix: photo size & position

--- app/page.jsx
+++ app/page.jsx
@@ -4,13 +4,14 @@ import { Button } from "@/components/ui/button";
 //components
 import SocialMedia from "@/components/SocialMedia";
 import Photo from "@/components/Photo";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24 xl:items-start">
-          <div className="text-center xl:text-left">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:my-6 xl:items-start">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Fullstack Developer</span>
             <h1 className="h2 mb-6">
               Hello, I'm <br />
@@ -24,14 +25,14 @@ export default function Home() {
 
             {/* button cv & social media */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="md"
-                className="uppercase flex items-center gap-2"
+              <Link
+                href="https://drive.google.com/file/d/1U36SleFUbHMfCJHuxTtisBYOemGRBZ9I/view"
+                target="_blank"
+                className="uppercase flex items-center gap-2 border border-accent rounded-full py-2 px-6 hover:bg-accent hover:text-primary hover:transition-all duration-500"
               >
                 <span>See CV</span>
                 <FiEye className="text-xl" />
-              </Button>
+              </Link>
               <div className="mb-8 xl:mb-0">
                 <SocialMedia
                   containerStyles="flex gap-6"
@@ -42,7 +43,7 @@ export default function Home() {
           </div>
 
           {/* Photo Ravi */}
-          <div>
+          <div className="mx-auto order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
